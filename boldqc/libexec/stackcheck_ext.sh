5d2b08669f8017b3a174c7d8fbb119cef3d9de73
Kyle Kurkela <kkurkela@bu.edu>
2026-04-03 14:12:56 -0400

    bug fix -- positive number followed by negative numbers do not get parsed correctly. Adding a space here fixes the problem.

--- boldqc/libexec/stackcheck_ext.sh
+++ boldqc/libexec/stackcheck_ext.sh
@@ -344,7 +344,19 @@ checkMode "$outdir/$base.rdat" "c"
 
 tail -n $numof_tps $outdir/moco.par | awk 'BEGIN {n=0; init=0;} ($1 !~/#/) { ncol = NF; init = 1; } (init == 1 && $1 !~/#/) { if (NF != ncol) { print "format error"; exit -1;} for (j=1;j<=ncol;j++) data[n,int((j+2)%6) + 1] = $j; n++;} END { for (i = 0; i < n; i++) { printf("%d", i+1); for (j = 1;j <=ncol; j++) printf ("%10.6f", data[i,j]); printf("%10.6f\n",1);}}' > $outdir/$base.dat
 tail -n $numof_tps $outdir/moco.par | awk 'BEGIN {n=0; init=0;} ($1 !~/#/) { ncol = NF; if (init == 0) {for (j = 1; j <= ncol; j++) { data_average[j] = 0;}; }; init = 1; } (init == 1 && $1 !~/#/) { if (NF != ncol) { print "format error"; exit -1;} for (j=1;j<=ncol;j++) {data[n,int((j+2)%6)+1] = $j; data_average[int((j+2)%6)+1] = (data[n,int((j+2)%6+1)] + data_average[int((j+2)%6+1)]*(n+1) ) / (n+2);} n++;} END { for (i = 0; i < n; i++) { printf("%d", i+1); for (j = 1;j <=ncol; j++) printf ("%10.6f", data[i,j] - data_average[j]); printf("%10.6f\n", 1);}}' > $outdir/$base.rdat
-tail -n $numof_tps $outdir/moco.par | awk 'BEGIN {n=0; init=0;} ($1 !~/#/) { ncol = NF; if (init == 0) {printf ("%d",1); for (j = 1; j <= ncol; j++) { printf("%10.6f", 0)}; printf("%10.6f\n",1); }; init = 1; } (init == 1 && $1 !~/#/) { if (NF != ncol) { print "format error"; exit -1;} for (j=1;j<=ncol;j++) data[n,int((j+2)%6)+1] = $j; n++;} END { for (i = 1; i < n; i++) { printf("%d", i+1); for (j = 1;j <=ncol; j++) printf ("%10.6f", data[i,j] - data[i-1,j]); printf("%10.6f\n", 0);}}' > $outdir/$base.ddat
+tail -n $numof_tps $outdir/moco.par | awk 'BEGIN {n=0; init=0;} 
+  ($1 !~/#/) { ncol = NF; init = 1; } 
+  (init == 1 && $1 !~/#/) { 
+    if (NF != ncol) { print "format error"; exit -1;} 
+    for (j=1;j<=ncol;j++) data[n,int((j+2)%6) + 1] = $j; n++;
+  } 
+  END { 
+    for (i = 0; i < n; i++) { 
+      printf("%d", i+1); 
+      for (j = 1;j <=ncol; j++) printf (" %10.6f", data[i,j]);  # <-- space added
+      printf(" %10.6f\n",1);                                      # <-- space added
+    }
+  }' > $outdir/$base.ddat
 echo "Done."
 
 echo -n "Calculating motion parameters... "
